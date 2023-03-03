dd179e94dac1f2c974bf8deb0fea2ff39ad9bd46
Duan Chaofan <dchaof888@163.com>
2023-03-03 12:21:53 +0800

    个人中心ui完成

---
+++ components/icons-card/icons-card.vue
@@ -0,0 +1,34 @@
+<template>
+	<!-- 个人中心中间的icon图标 -->
+	<view>
+		<view class="px-3">
+			<view class="flex bg-white shadow rounded ">
+				<view hover-class="bg-light" class="py-3 flex flex-column align-center justify-center flex-1" v-for="(item,index) in icons" :key="index">
+					<text class="iconfont font-weight-bolder" :class="item.icon" style="font-size: 20px;color: #febd00;"></text>
+					<text class="font-sm mt-1">{{item.name}}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"icons-card",
+		props:{
+			icons:{
+				type:Array,
+				default:() => []
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- pages/tabbar/home/home.vue
+++ pages/tabbar/home/home.vue
@@ -1,7 +1,28 @@
 <template>
 	<view>
-		<button @click="handleLogin" type="primary">登录</button>
-		<view>{{user ? user.username : '未登录'}}</view>
+		<view class="home-bg position-relative">
+			<view class="flex px-3 py-4" >
+				<image src="/static/uni.png" style="width:120rpx;height:120rpx;border-radius: 50%;"></image>
+				<view class="flex flex-column justify-center align-left text-white ml-3">
+					<text>立即登录</text>
+					<text class="font-sm mt-2">登录解锁更多功能</text>
+				</view>
+			</view>
+			
+			<icons-card :icons="icons"></icons-card>
+			
+			<view class="px-3">
+				<uni-list-item clickable title="我的优惠券" showArrow class="py-1">
+					<text slot="header" class="iconfont icon-9 mr-2" style="font-size: 20px;color: #4396ec;"></text>
+				</uni-list-item>
+				<uni-list-item clickable title="常见问题" showArrow class="py-1">
+					<text slot="header" class="iconfont icon-help mr-2" style="font-size: 20px;color: #4396ec;"></text>
+				</uni-list-item>
+				<uni-list-item clickable title="设置" showArrow class="py-1">
+					<text slot="header" class="iconfont icon-leimupinleifenleileibie mr-2" style="font-size: 20px;color: #4396ec;"></text>
+				</uni-list-item>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -10,7 +31,20 @@
 	export default {
 		data() {
 			return {
-				user:null
+				icons:[{
+					icon:"icon-OrderHistory",
+					name:"订单"
+				},{
+					icon:"icon-pinglun2",
+					name:"消息"
+				},{
+					icon:"icon-shoucang1",
+					name:"收藏"
+				},{
+					icon:"icon-e-learning-monitor",
+					name:"在学",
+					type:"switchTab"
+				}]
 			};
 		},
 		created(){ 
@@ -30,5 +64,10 @@
 </script>
 
 <style lang="scss">
-
+	.home-bg {
+		height: 310rpx;
+		background-color: #5ccc84;
+		border-bottom-right-radius: 30rpx;
+		border-bottom-left-radius: 30rpx;
+	}
 </style>
